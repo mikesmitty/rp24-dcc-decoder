f41bb9b01d2a04fa142c515146ca038c65a721c3
Michael Smith <mikejsmitty@gmail.com>
2025-02-23 10:56:59 -0500

    chore: unify and automate gerber scripts

--- hardware/export.sh
+++ hardware/export.sh
@@ -6,9 +6,18 @@ PCB_DIR="${SCRIPT_DIR}/${PCB}"
 OUTPUT=${PCB_DIR}/production
 
 if [ -z "$PCB" ]; then
-    echo "Usage: $0 <pcb-name>"
+    echo "Usage: $0 <pcb-name> [panel-option]"
     exit 1
 fi
 
+if [ -n "$2" ]; then
+    BATCH_MOD="-p ${PCB_DIR}/panelize-${2}.json"
+fi
+
 mkdir -p "$OUTPUT"
-kikit fab jlcpcb --no-drc --assembly --schematic "${PCB_DIR}/${PCB}.kicad_sch" "${PCB_DIR}/${PCB}.kicad_pcb" "$OUTPUT/"
+if [ -e "${PCB_DIR}/panelize.json" ]; then
+    kikit panelize -p "${PCB_DIR}/panelize.json" $BATCH_MOD -p :jlcTooling "${PCB_DIR}/${PCB}.kicad_pcb" "${OUTPUT}/${PCB}.kicad_pcb"
+    kikit fab jlcpcb --no-drc --assembly --autoname --schematic "${PCB_DIR}/${PCB}.kicad_sch" "${OUTPUT}/${PCB}.kicad_pcb" "$OUTPUT/"
+else
+    kikit fab jlcpcb --no-drc --assembly --autoname --schematic "${PCB_DIR}/${PCB}.kicad_sch" "${PCB_DIR}/${PCB}.kicad_pcb" "$OUTPUT/"
+fi
