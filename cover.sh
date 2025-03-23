16780dfde0a5ccb91ba27bb237c0a8e530186aa6
Michael Smith <mikejsmitty@gmail.com>
2025-03-23 11:25:58 -0400

    chore: add workflow for build testing

--- cover.sh
+++ cover.sh
@@ -3,5 +3,5 @@
 rm -fv cover.out cover.html
 GOCACHE=$(mktemp -d)
 go env -w GOCACHE=$GOCACHE
-go test -v -coverprofile cover.out ./pkg/... && go tool cover -html cover.out -o cover.html && open cover.html
+go test -v -coverprofile cover.out ./... && go tool cover -html cover.out -o cover.html && open cover.html
 go env -u GOCACHE
